1b0093efedd3e91f43a0d197352376fc348ed25a
TheSamozvanec <semov777@mail.ru>
2025-06-06 11:01:11 +0300

    поправка

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,37 @@
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
-const users:{id:string,name:string,age:number,email:string}[]=[
-  { id:'Oa_25_olya_mbllo',
+import UsersMap from './components/UsersMap';
+interface User {
+  id:string;
+  name:string;
+  age:number;
+  email:string
+}
+export interface IUsers {
+  users:User[]
+}
+const users:User[]=[
+  { 
+    id:'Oa_25_olya_mbllo',
     name:'Olya',
     age:25,
     email:'olya777@mbllo.net'
   },
-  { id:'Va_30_vasan_mblla_net',
+  { 
+    id:'Va_30_vasan_mblla_net',
     name:'Vasya',
     age:30,
     email:'vasan@mblla_net.ru'
   },
-  { id:'Nj_36_nikola555_neyandex',
+  { 
+    id:'Nj_36_nikola555_neyandex',
     name:'Nikolaj',
     age:36,
     email:'nikola555@neyandex.com'
   },
-  { id:'Ir_32_nifigasebe_rabler',
+  { 
+    id:'Ir_32_nifigasebe_rabler',
     name:'Igor',
     age:40,
     email:'igogor13@mailllo.kz'
@@ -29,13 +43,7 @@ function App() {
 
   return (
     <>
-    {users.map((user:{id:string,name:string,age:number,email:string})=>(
-      <div key={user.id}>
-        <p>name: {user.name}</p>
-        <p>age: {user.age}</p>
-        <p>email: {user.email}</p>
-      </div>
-    ))}  
+      <UsersMap  users={users}/>
     </>
   )
 }
---
+++ src/components/UsersMap.tsx
@@ -0,0 +1,17 @@
+//import React from "react";
+import type { IUsers } from "../App";
+
+const UsersMap = ({users}:IUsers)=>{
+    return (
+        <>
+            {users.map((user:{id:string,name:string,age:number,email:string})=>(
+                <div key={user.id}>
+                    <p>name: {user.name}</p>
+                    <p>age: {user.age}</p>
+                    <p>email: {user.email}</p>
+                </div>
+    ))}  
+        </>
+    )
+}
+export default UsersMap
